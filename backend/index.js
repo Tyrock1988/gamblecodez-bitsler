e3e66f3f0d7df278ae63559d1e513d54da92217d
GAMBLEODEZ aka Tyler <thetylo88@gmail.com>
2025-06-28 03:55:23 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
